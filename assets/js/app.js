41804d97db677261d91b92780afbf830d83ffd6c
William Oliveira <sudowilliam@gmail.com>
2013-12-24 14:45:13 -0200

    routes and templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,33 @@
-var App = angular.module('App', []);
+var App = angular.module('App', ['ngRoute']);
 
-function CustomerController($scope) {
+App.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', 
+      {
+        controller: 'CustomerController',
+        templateUrl: 'templates/customer.html'
+      }
+    )
+    .when('/customers', 
+      {
+        controller: 'CustomerController',
+        templateUrl: 'templates/city.html'
+      }
+    )
+    .otherwise( { redirectTo: '/' } );
+});
+
+function CustomerController ($scope) {
   $scope.customers = [
     { name : 'Steve Jobs', city : 'San Francisco' },
     { name : 'Bill Gates', city : 'Medina' },
-    { name : 'Mark Zuckerberg', city : 'Palo Alto' }
+    { name : 'Mark Zuckerberg', city : 'Palo Alto' },
+    { name : 'Lucy the Samoyed', city : 'Governador Valadares' }
   ];
+
+  $scope.addCustomer = function () {
+    $scope.customers.push( { name : $scope.newCustomer.name, city : $scope.newCustomer.city } );
+  }
 }
 
 App.controller('CustomerController', CustomerController);
