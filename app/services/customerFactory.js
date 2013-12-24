6f54c5725c2f17e01f77ee46447fd47fba58a13d
William Oliveira <sudowilliam@gmail.com>
2013-12-24 18:29:20 -0200

    visual improvements

--- app/services/customerFactory.js
+++ app/services/customerFactory.js
@@ -4,7 +4,9 @@ App.factory('CustomerFactory', function () {
     { name : 'Steve Jobs', city : 'San Francisco' },
     { name : 'Bill Gates', city : 'Medina' },
     { name : 'Mark Zuckerberg', city : 'Palo Alto' },
-    { name : 'Lucy the Samoyed', city : 'Governador Valadares' }
+    { name : 'Lucy the Samoyed', city : 'Governador Valadares' },
+    { name : 'William Oliveira', city : 'Governador Valadares' },
+    { name : 'Juliana Oliveira', city : 'Governador Valadares' }
   ];
 
   factory.getCustomers = function () {
