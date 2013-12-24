1550b3208366117d32c4e44f5edcf9bdb54025fa
William Oliveira <sudowilliam@gmail.com>
2013-12-24 15:20:44 -0200

    adds factory

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,17 +17,25 @@ App.config(function ($routeProvider) {
     .otherwise( { redirectTo: '/' } );
 });
 
-function CustomerController ($scope) {
-  $scope.customers = [
+App.factory('CustomerFactory', function () {
+  var factory = {};
+  var customers = [
     { name : 'Steve Jobs', city : 'San Francisco' },
     { name : 'Bill Gates', city : 'Medina' },
     { name : 'Mark Zuckerberg', city : 'Palo Alto' },
     { name : 'Lucy the Samoyed', city : 'Governador Valadares' }
   ];
 
+  factory.getCustomers = function () {
+    return customers;
+  }
+  return factory;
+});
+
+App.controller('CustomerController', function ($scope, CustomerFactory) {
+  $scope.customers = CustomerFactory.getCustomers();
+
   $scope.addCustomer = function () {
     $scope.customers.push( { name : $scope.newCustomer.name, city : $scope.newCustomer.city } );
   }
-}
-
-App.controller('CustomerController', CustomerController);
+});
