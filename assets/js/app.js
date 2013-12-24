2c8a01f675ef55b5b73043bffe4f663aef679dba
William Oliveira <sudowilliam@gmail.com>
2013-12-24 02:27:51 -0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+function CustomerController($scope) {
+  $scope.customers = [
+    { name : 'Steve Jobs', city : 'San Francisco' },
+    { name : 'Bill Gates', city : 'Medina' },
+    { name : 'Mark Zuckerberg', city : 'Palo Alto' }
+  ];
+}
